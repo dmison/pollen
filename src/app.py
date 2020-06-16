38f808e06f58c7d1c78f9d54ad08de28ce379c6e
Dana Jadzia Mison <dana.jadzia.mison@gmail.com>
2020-06-17 09:37:02 +1000

    using tornadose

--- src/app.py
+++ src/app.py
@@ -1,48 +1,35 @@
 import os.path
-import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-import tornado.web
+from tornado import httpserver, ioloop, web
 from tornado.options import define, options
-from SSE import Handler
-from tornado.iostream import StreamClosedError
-import asyncio
+from tornadose.handlers import EventSource
+from tornadose.stores import QueueStore
+
+store = QueueStore()
 
 define("port", default=8000, help="run on the given port", type=int)
 define("debug", default=False, help="run in debug mode", type=bool)
 
 
-class IndexHandler(tornado.web.RequestHandler):
-    async def get(self):
-        greeting = self.get_argument('greeting', 'Hello')
-        self.write(greeting + ', friendly user!')
+class AddHandler(web.RequestHandler):
 
-
-class EventHandler(Handler.Handler):
-    async def get(self):
-        while True:
-            try:
-                self.emit('hello', 'there')
-                await asyncio.sleep(1)
-            except StreamClosedError:
-                pass
+    async def post(self):
+        store.submit(self.request.body)
 
 
 if __name__ == "__main__":
-    tornado.options.parse_command_line()
+    options.parse_command_line()
     static_path = os.path.abspath(
         "static/"
     )
-    app = tornado.web.Application(
+    app = web.Application(
         debug=options.debug,
         handlers=[
-            (r"/rest/(.*)", IndexHandler),
-            (r"/date", EventHandler),
-            (r"/(index\.html)", tornado.web.StaticFileHandler, dict(path=static_path)),
-
+            (r"/add", AddHandler),
+            (r"/events", EventSource, {'store': store}),
+            (r"/(index\.html)", web.StaticFileHandler, dict(path=static_path)),
         ]
     )
 
-    http_server = tornado.httpserver.HTTPServer(app)
+    http_server = httpserver.HTTPServer(app)
     http_server.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+    ioloop.IOLoop.instance().start()
